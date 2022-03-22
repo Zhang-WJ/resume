5fe8497d7383b8934da0fd62641bd9fac2c86f09
zhangweijun <ahnqihyun@sina.cn>
2022-03-22 14:02:47 +0800

    add lifecycle

--- src/App/RandomNum.purs
+++ src/App/RandomNum.purs
@@ -4,6 +4,7 @@ import Prelude
 
 import Data.Maybe (Maybe(..), maybe)
 import Effect.Class (class MonadEffect)
+import Effect.Class.Console (log)
 import Effect.Random (random)
 import Halogen as H
 import Halogen.HTML as HH
@@ -11,7 +12,10 @@ import Halogen.HTML.Events as HE
 
 type State = Maybe Number
 
-data Action = Regenerate
+data Action = 
+      Initialize
+    | Regenerate
+    | Finalize
 
 component :: forall query input output m. MonadEffect m => H.Component query input output m
 component = 
@@ -19,7 +23,12 @@ component =
         {
             initialState,
             render,
-            eval: H.mkEval $ H.defaultEval {handleAction = handleAction}
+            eval: H.mkEval $ H.defaultEval 
+                {
+                    handleAction = handleAction
+                    , initialize = Just Initialize
+                    , finalize = Just Finalize    
+                }
         }
 
 initialState :: forall input. input -> State
@@ -40,8 +49,15 @@ render state = do
 
 handleAction :: forall output m.MonadEffect m => Action -> H.HalogenM State Action () output m Unit
 handleAction = case _ of
+    Initialize -> do
+      handleAction Regenerate
+      newNumber <- H.get
+      log ("Initialize: " <> show newNumber)
+
     Regenerate -> do
       newNumber <- H.liftEffect random
       H.modify_ \_ -> Just newNumber
 
-      
+    Finalize -> do
+      number <- H.get
+      log ("Finalized! Last number was: " <> show number)
