34614e6ef642c14f648eb2bdafb1b7e059aaadc6
zhangweijun <ahnqihyun@sina.cn>
2022-03-23 16:47:38 +0800

    add subscriber

--- src/Main.purs
+++ src/Main.purs
@@ -5,6 +5,7 @@ import Prelude
 import App.BasicComponent as Counter
 import App.Button as Button
 import App.RandomNum as RandomNum
+import App.Timer as Timer
 import Effect (Effect)
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
@@ -12,4 +13,7 @@ import Halogen.VDom.Driver (runUI)
 main :: Effect Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
-  runUI RandomNum.component unit body
+  _ <- runUI RandomNum.component unit body
+  _ <- runUI Button.component unit body
+  _ <- runUI Timer.component unit body
+  runUI Counter.component unit body
